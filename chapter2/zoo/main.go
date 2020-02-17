dce11945a2b4b41f6d684e7227052cd4774b7b17
Ric <cat.to.love418@gmail.com>
2020-02-18 08:23:22 +0900

    add animals

---
+++ chapter2/zoo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Ric418/go_playground/chapter2/zoo/animals"
+)
+
+func main() {
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.RabbitFeed())
+}
